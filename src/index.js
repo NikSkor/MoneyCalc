b63b66c7496194e6059d500a55b6c9e7fb4ada24
Nikita Skorodumov <mr-zerx@yandex.ru>
2020-08-13 22:34:59 +0500

    Classes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import ReactDOM, { render } from 'react-dom';
 import App from './App';
+
+const FunctionComponents = () => 
+  React.createElement('h1', null, 'Greetings!');
+
+const Element = React.createElement('p', null, "Paragraph");
+
+class ClassComponents extends Component {
+  render() {
+    return React.createElement('div', {className: 'main'}, 
+      null, React.createElement(FunctionComponents), Element)
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ClassComponents />
   </React.StrictMode>,
   document.getElementById('root')
 );
