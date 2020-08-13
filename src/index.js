37d6c7cb3825b7d4f097d7396c41a34ec0415855
Nikita Skorodumov <mr-zerx@yandex.ru>
2020-08-14 00:14:15 +0500

    Вёрстка

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM, { render } from 'react-dom';
 import App from './App';
+import './normalize.css';
+import './style.css';
 
 ReactDOM.render(
   <React.StrictMode>
