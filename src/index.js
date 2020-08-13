02b29732c9754b93447e8500afd8192e1b4d83e3
Nikita Skorodumov <mr-zerx@yandex.ru>
2020-08-13 23:10:14 +0500

    Особенности jsx

--- src/index.js
+++ src/index.js
@@ -2,21 +2,24 @@ import React, {Component} from 'react';
 import ReactDOM, { render } from 'react-dom';
 import App from './App';
 
-const FunctionComponents = (props) => 
-  React.createElement('h1', {id: 'hi'}, props.text);
+const Header = (props) => <h1 id='hi'>{props.text}</h1>;
 
-const Element = React.createElement('p', null, "Paragraph");
+const Element = <p>Paragraph</p>;
 
-class ClassComponents extends Component {
+class Main extends Component {
   render() {
-    return React.createElement('div', {className: 'main'}, 
-      React.createElement(FunctionComponents, {text: 'свойство текст'}), Element)
+    return(
+      <div className='main'>
+        <Header text={"свойство ТЕКСТ " + this.props.count}/>
+        {Element}
+      </div>
+    ) 
   }
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <ClassComponents />
+    <Main count={7}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
