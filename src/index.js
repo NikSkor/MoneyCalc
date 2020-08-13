f5eeef517df7ce80834b493f5df783c4694a0293
Nikita Skorodumov <mr-zerx@yandex.ru>
2020-08-13 23:13:27 +0500

    Начало

--- src/index.js
+++ src/index.js
@@ -2,24 +2,9 @@ import React, {Component} from 'react';
 import ReactDOM, { render } from 'react-dom';
 import App from './App';
 
-const Header = (props) => <h1 id='hi'>{props.text}</h1>;
-
-const Element = <p>Paragraph</p>;
-
-class Main extends Component {
-  render() {
-    return(
-      <div className='main'>
-        <Header text={"свойство ТЕКСТ " + this.props.count}/>
-        {Element}
-      </div>
-    ) 
-  }
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <Main count={7}/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
